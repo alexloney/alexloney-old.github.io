e13990c834ba4cb32012930c84e8cc14774dc915
Alex Loney <loney.alex@gmail.com>
2022-06-24 11:30:32 -0700

    Updating with older solves

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,15 +5,57 @@ var store = [{
         "url": "/blog/new-blog-who-dis/",
         "teaser": null
       },{
+        "title": "HTB: Cap Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration I began searching this box with a standard nmap scan: $ sudo nmap -sC -sV -oA nmap/cap 10.129.121.64 Starting Nmap 7.91 ( https://nmap.org ) at 2021-06-06 21:26 EDT...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb easy","offsec"],
+        "url": "/blog/writeup/htb/htb-cap/",
+        "teaser": null
+      },{
+        "title": "HTB: Ophiuchi Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration nmap -sC -sV -oA nmap/ophiuchi 10.10.10.227 Starting Nmap 7.91 ( https://nmap.org ) at 2021-06-11 15:39 EDT Nmap scan report for 10.10.10.227 Host is up (0.078s latency). Not shown:...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb medium","offsec"],
+        "url": "/blog/writeup/htb/htb-ophiuchi/",
+        "teaser": null
+      },{
         "title": "HTB: Spectra Writeup",
         "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration I began this box with a standard nmap scan: $ nmap -sC -sV -oA nmap/spectra 10.10.10.229 Starting Nmap 7.91 ( https://nmap.org ) at 2021-06-08 13:01 EDT Nmap scan...","categories": ["blog","writeup","htb"],
         "tags": ["htb","htb easy","offsec"],
         "url": "/blog/writeup/htb/htb-spectra/",
         "teaser": null
       },{
+        "title": "HTB: Armageddon Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration nmap As always, beginning with an nmap of the box to determine what is open $ cat nmap/armageddon.nmap # Nmap 7.91 scan initiated Tue Jun 8 18:06:58 2021...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb easy","offsec"],
+        "url": "/blog/writeup/htb/htb-armageddon/",
+        "teaser": null
+      },{
+        "title": "HTB: Love Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration I began by the standard enumeration flow, using nmap # Nmap 7.91 scan initiated Wed Jun 9 13:19:10 2021 as: nmap -sC -sV -oA nmap/love 10.10.10.239 Nmap scan...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb easy","offsec"],
+        "url": "/blog/writeup/htb/htb-love/",
+        "teaser": null
+      },{
         "title": "HTB: Tenet Writeup",
         "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration Beginning this box as every box, with a nmap scan of the box to locate open ports. $ sudo nmap -sC -sV -oA nmap/tenet 10.10.10.223 Starting Nmap 7.91...","categories": ["blog","writeup","htb"],
         "tags": ["htb","htb medium","offsec"],
         "url": "/blog/writeup/htb/htb-tenet/",
+        "teaser": null
+      },{
+        "title": "HTB: Knife Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! Enumeration I began the same as always, with an nmap scan # Nmap 7.91 scan initiated Fri Jun 11 13:42:53 2021 as: nmap -sC -sV -oA nmap/knife 10.10.10.242 Nmap...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb easy","offsec"],
+        "url": "/blog/writeup/htb/htb-knife/",
+        "teaser": null
+      },{
+        "title": "HTB: Dynstr Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Cap. Stop reading here if you do not want spoilers!!! This is by FAR the most difficult box that I’ve done on HTB (I haven’t done any hard or insane boxes, but this was harder than any other medium...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb medium","offsec"],
+        "url": "/blog/writeup/htb/htb-dynstr/",
+        "teaser": null
+      },{
+        "title": "HTB: Trick Writeup",
+        "excerpt":"There are spoilers below for the Hack The Box box named Trick. Stop reading here if you do not want spoilers!!! This box was listed as “easy” however I had more trouble with it than other easy boxes I’ve done. Enumeration I began as always with an nmap port scan...","categories": ["blog","writeup","htb"],
+        "tags": ["htb","htb easy","offsec"],
+        "url": "/blog/writeup/htb/htb-trick/",
         "teaser": null
       }]
